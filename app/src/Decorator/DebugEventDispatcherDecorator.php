146d07d1accb32a979301dbd18f9f5bf05535f57
vpramatarov <velizarpamatarov@gmail.com>
2022-12-25 10:40:46 +0200

    add DebugEventDispatcherDecorator for debugging Events

---
+++ app/src/Decorator/DebugEventDispatcherDecorator.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Decorator;
+
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+#[AsDecorator('event_dispatcher')]
+class DebugEventDispatcherDecorator implements EventDispatcherInterface
+{
+    public function __construct(private readonly EventDispatcherInterface $eventDispatcher) {}
+
+    public function dispatch(object $event, string $eventName = null): object
+    {
+        dump('--------------------');
+        dump('Dispatching event: ' . $event::class);
+        dump('--------------------');
+
+        return $this->eventDispatcher->dispatch($event, $eventName);
+    }
+
+    public function addListener(string $eventName, $listener, int $priority = 0)
+    {
+        $this->eventDispatcher->addListener($eventName, $listener, $priority);
+    }
+
+    public function addSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->eventDispatcher->addSubscriber($subscriber);
+    }
+
+    public function removeListener(string $eventName, $listener)
+    {
+        $this->eventDispatcher->removeListener($eventName, $listener);
+    }
+
+    public function removeSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->eventDispatcher->removeSubscriber($subscriber);
+    }
+
+    public function getListeners(string $eventName = null): array
+    {
+        return $this->eventDispatcher->getListeners($eventName);
+    }
+
+    public function getListenerPriority(string $eventName, $listener): ?int
+    {
+        return $this->eventDispatcher->getListenerPriority($eventName, $listener);
+    }
+
+    public function hasListeners(string $eventName = null): bool
+    {
+        return $this->eventDispatcher->hasListeners($eventName);
+    }
+}
