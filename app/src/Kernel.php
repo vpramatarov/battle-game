95cd84719d270ba3cbf01b45671bc1f24fd454ef
vpramatarov <velizarpamatarov@gmail.com>
2022-12-25 10:35:35 +0200

    add CompilerPass in Kernel for game subscriber interfaces

---
+++ app/src/Kernel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use App\Observer\GameObserverInterface;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel implements CompilerPassInterface
+{
+    use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container)
+    {
+        $container->registerForAutoconfiguration(GameObserverInterface::class)
+            ->addTag('game.observer');
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->findDefinition(GameApplication::class);
+        $taggedObservers = $container->findTaggedServiceIds('game.observer');
+        foreach ($taggedObservers as $id => $tags) {
+            $definition->addMethodCall('subscribe', [new Reference($id)]);
+        }
+    }
+}
